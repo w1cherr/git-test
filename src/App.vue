3803f646093a787504bef84e36723545488a57a0
熊俊祥 <>
2019-10-14 11:26:14 +0800

    b分支开发了一些代码并提交，有公用的也有特别的

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/" class="bClass">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <div>这里是b分支</div>
     </div>
     <router-view/>
   </div>
