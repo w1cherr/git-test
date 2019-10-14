37a8d88af9a44e422f49729d064ff1f84691e411
熊俊祥 <>
2019-10-14 12:45:36 +0800

    b分支新增了一些代码

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <div>这里是公共代码</div>
     </div>
     <router-view/>
   </div>
