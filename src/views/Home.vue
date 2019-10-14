1e96093df685664c8374e34a44c809d063b53103
熊俊祥 <>
2019-10-14 15:47:00 +0800

    提交了一些公共代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" class="commonClass">
     <HelloWorld msg="Welcome to Your Vue.js App" class="commonClass"/>
+    <div>提交了一些公共代码上去</div>
     <div>这里是a分支</div>
     <div>这里是a分支2</div>
   </div>
